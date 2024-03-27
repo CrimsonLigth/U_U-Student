867c1c0fe32bddb9d04b080d8a0dc47d15fba58a
CrimsonLigth <164855920+CrimsonLigth@users.noreply.github.com>
2024-03-27 09:07:43 +0300

    Add files via upload

--- Дополнительные задания 1.2.py
+++ Дополнительные задания 1.2.py
@@ -1,34 +1,8 @@
 weight = int(input('Введите вес посылки в кг '))
-coast = 20
+coast = 50
 if weight < 2:
     print('Стоимость доставки', coast, 'рублей')
-if weight == 2:
-    coast += 20
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 3:
-    coast += 40
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 4:
-    coast += 60
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 5:
-    coast += 80
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 6:
-    coast += 100
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 7:
-    coast += 120
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 8:
-    coast += 140
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 9:
-    coast += 160
-    print('Стоимость доставки', coast, 'рублей')
-if weight == 10:
-    coast += 180
-    print('Стоимость доставки', coast, 'рублей')
-if weight > 10:
-    coast = 200
-    print('Стоимость доставки', coast, 'рублей')
+if weight > 2 and weight <= 10:
+    print('Стоимость доставки', coast + (weight - 2) * 20, 'рублей')
+else:
+    print('Стоимость доставки', 200, 'рублей')
